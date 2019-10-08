3cf9f3016caebacc079463166512732cfe4acf5e
madsh <madsh@158.37.188.105>
2019-10-08 12:26:09 +0200

    Start oppgåve 1

--- src/no/hvl/dat100ptc/oppgave1/GPSPoint.java
+++ src/no/hvl/dat100ptc/oppgave1/GPSPoint.java
@@ -4,74 +4,79 @@ import no.hvl.dat100ptc.TODO;
 
 public class GPSPoint {
 
-	// TODO - objektvariable
-		
-	public GPSPoint(int time, double latitude, double longitude, double elevation) {
-
-		// TODO - konstruktur
-
-		throw new UnsupportedOperationException(TODO.construtor("GPSPoint"));
-
-	}
-
-	// TODO - get/set metoder
-	public int getTime() {
-		
-		throw new UnsupportedOperationException(TODO.method());
-		
-	}
-
-	public void setTime(int time) {
-				
-		throw new UnsupportedOperationException(TODO.method());
-
-	}
-
-	public double getLatitude() {
-		
-		throw new UnsupportedOperationException(TODO.method());
-		
-	}
-
-	public void setLatitude(double latitude) {
-		
-		throw new UnsupportedOperationException(TODO.method());
-		
-	}
-
-	public double getLongitude() {
-		
-		throw new UnsupportedOperationException(TODO.method());
-		
-	}
-
-	public void setLongitude(double longitude) {
-		
-		throw new UnsupportedOperationException(TODO.method());
-		
-	}
-
-	public double getElevation() {
-		
-		throw new UnsupportedOperationException(TODO.method());
-		
-	}
-
-	public void setElevation(double elevation) {
-		
-		throw new UnsupportedOperationException(TODO.method());
-		
-	}
-	
-	public String toString() {
-		
-		String str;
-		
-		// TODO - start
-
-		throw new UnsupportedOperationException(TODO.method());
-
-		// TODO - slutt
+	private int time;
+    private double latitude;
+    private double longitude;
+    private double elevation;
+       
+    public GPSPoint(int time, double latitude, double longitude, double elevation) {
+ 
+        // TODO - konstruktur
+       
+        this.time = time;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.elevation = elevation;
+ 
+       
+ 
+    }
+ 
+    // TODO - get/set metoder
+    public int getTime() {
+       
+        return time;
+       
+       
+    }
+ 
+    public void setTime(int time) {
+               
+        this.time = time;
+ 
+    }
+ 
+    public double getLatitude() {
+       
+        return latitude;
+       
+    }
+ 
+    public void setLatitude(double latitude) {
+       
+        this.latitude = latitude;
+       
+    }
+ 
+    public double getLongitude() {
+       
+        return longitude;
+       
+    }
+ 
+    public void setLongitude(double longitude) {
+       
+        this.longitude = longitude;
+       
+    }
+ 
+    public double getElevation() {
+       
+        return elevation;
+       
+    }
+ 
+    public void setElevation(double elevation) {
+       
+        this.elevation = elevation;
+       
+    }
+   
+    public String toString() {
+       
+        String str = time + " " + "(" + latitude + "," + longitude + ")" + " " + elevation + "\n";
+       
+        return str;
 		
 	}
 }
